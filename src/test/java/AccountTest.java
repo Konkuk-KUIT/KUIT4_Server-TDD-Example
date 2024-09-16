5718f4fdab93ec724963baa7c0dca371c1b5eca5
hamhyeongju <gudwn357@gmail.com>
2024-09-16 11:06:25 +0900

    Assertions static import

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,27 +1,26 @@
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class AccountTest {
 
     @Test
     void 계좌_생성() {
         Account account = new Account();
-        Assertions.assertThat(account.getBalance()).isEqualTo(0);
+        assertThat(account.getBalance()).isEqualTo(0);
     }
 
     @Test
     void 계좌_생성_1000() {
         Account account = new Account(NaturalNumber.from(1000));
-        Assertions.assertThat(account.getBalance()).isEqualTo(1000);
+        assertThat(account.getBalance()).isEqualTo(1000);
     }
 
     @Test
     void 계좌_입금_확인() {
         Account account = new Account(NaturalNumber.from(10000));
         account.deposit(NaturalNumber.from(1000));
-        Assertions.assertThat(account.getBalance()).isEqualTo(11000);
+        assertThat(account.getBalance()).isEqualTo(11000);
     }
 
 }
