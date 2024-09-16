84ae56fcf22814aff009919e635d34eb8d932dad
hamhyeongju <gudwn357@gmail.com>
2024-09-16 10:47:01 +0900

    AccountTest 계좌 생성 테스트 코드 추가

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,5 +1,20 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountTest {
+
+    @Test
+    void 계좌_생성() {
+        Account account = new Account();
+        Assertions.assertThat(account.getBalance()).isEqualTo(0);
+    }
+
+    @Test
+    void 계좌_생성_1000() {
+        Account account = new Account(1000);
+        Assertions.assertThat(account.getBalance()).isEqualTo(1000);
+    }
 
 }
