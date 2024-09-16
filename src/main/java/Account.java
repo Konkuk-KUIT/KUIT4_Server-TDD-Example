7692452770cd787287d103ea91167c50e895984f
hamhyeongju <gudwn357@gmail.com>
2024-09-16 10:47:38 +0900

    Account 계좌 생성 로직 추가

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,2 +1,16 @@
 public class Account {
+
+    private int balance;
+
+    public Account() {
+    }
+
+    public Account(int i) {
+
+        this.balance = i;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
 }
